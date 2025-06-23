a99b7f8a177d40a992b0958b616e98fe92f68c93
Renan Gomes <54034976+RGomes98@users.noreply.github.com>
2025-06-23 07:37:45 -0300

    style(layout): small adjustments to 'window.util.ts'

--- src/utils/window.util.ts
+++ src/utils/window.util.ts
@@ -1,7 +1,7 @@
 export function getInitialScale(width: number) {
   if (width <= 360) return 3.1;
   if (width <= 390) return 3.05;
-  if (width <= 414) return 3.15;
+  if (width <= 768) return 3.15;
   if (width <= 1366) return 1.2;
   if (width <= 1440) return 1.3;
   if (width <= 1536) return 1.15;
@@ -12,7 +12,7 @@ export function getInitialScale(width: number) {
 export function getZoomScale(width: number) {
   if (width <= 360) return 6;
   if (width <= 393) return 6.15;
-  if (width <= 414) return 5.95;
+  if (width <= 768) return 5.95;
   if (width <= 1366) return 3;
   if (width <= 1600) return 3.25;
   if (width <= 1920) return 3;
