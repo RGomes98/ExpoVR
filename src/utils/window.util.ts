aa2007f898758bd1fe60e868ad1f58ddfc3dc447
Renan Gomes <renango2015@gmail.com>
2025-06-24 00:09:13 -0300

    style(layout): small adjustments to 'window.util.ts'

--- src/utils/window.util.ts
+++ src/utils/window.util.ts
@@ -2,6 +2,8 @@ export function getInitialScale(width: number) {
   if (width <= 360) return 3.1;
   if (width <= 390) return 3.05;
   if (width <= 768) return 3.15;
+  if (width <= 1000) return 3.35;
+  if (width <= 1350) return 2;
   if (width <= 1366) return 1.2;
   if (width <= 1440) return 1.3;
   if (width <= 1536) return 1.15;
@@ -10,10 +12,10 @@ export function getInitialScale(width: number) {
 }
 
 export function getZoomScale(width: number) {
-  if (width <= 360) return 6;
-  if (width <= 393) return 6.15;
-  if (width <= 768) return 5.95;
-  if (width <= 1366) return 3;
-  if (width <= 1600) return 3.25;
-  if (width <= 1920) return 3;
+  if (width <= 360) return 5.25;
+  if (width <= 393) return 5.4;
+  if (width <= 768) return 5.2;
+  if (width <= 1366) return 2.25;
+  if (width <= 1600) return 2.5;
+  if (width <= 1920) return 2.25;
 }
